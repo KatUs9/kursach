fcf18398a4f85e477958ea1f1e10de772963878d
KatUs9 <ekaterina.us2003@mail.ru>
2024-01-09 23:33:06 +0300

    sql editor

--- src/lib/authorize.ts
+++ src/lib/authorize.ts
@@ -1,44 +1,44 @@
-import bcrypt from "bcrypt";
-import { eq } from "drizzle-orm";
+// import bcrypt from "bcrypt";
+// import { eq } from "drizzle-orm";
 
-import { db } from "@/lib/db";
-import { createJwtToken } from "@/lib/jwt";
-import { users } from "db/schema";
+// import { db } from "@/lib/db";
+// import { createJwtToken } from "@/lib/jwt";
+// import { users } from "db/schema";
 
-type AuthorizeResult =
-  | {
-      success: false;
-      message: string;
-    }
-  | {
-      success: true;
-      authToken: string;
-    };
+// type AuthorizeResult =
+//   | {
+//       success: false;
+//       message: string;
+//     }
+//   | {
+//       success: true;
+//       authToken: string;
+//     };
 
-export async function authorize(
-  login: string,
-  password: string
-): Promise<AuthorizeResult> {
-  const dbUsers = await db.select().from(users).where(eq(users.login, login));
+// export async function authorize(
+//   login: string,
+//   password: string
+// ): Promise<AuthorizeResult> {
+//   const dbUsers = await db.select().from(users).where(eq(users.login, login));
 
-  if (dbUsers.length != 1) {
-    return { success: false, message: "Невалидный логин или пароль" };
-  }
+//   if (dbUsers.length != 1) {
+//     return { success: false, message: "Невалидный логин или пароль" };
+//   }
 
-  const user = dbUsers[0];
+//   const user = dbUsers[0];
 
-  const isPasswordValid = await bcrypt.compare(password, user.password);
+//   const isPasswordValid = await bcrypt.compare(password, user.password);
 
-  if (!isPasswordValid) {
-    return { success: false, message: "Невалидный логин или пароль" };
-  }
+//   if (!isPasswordValid) {
+//     return { success: false, message: "Невалидный логин или пароль" };
+//   }
 
-  const authToken = createJwtToken(
-    {
-      userId: user.id
-    },
-    import.meta.env.JWT_SECRET
-  );
+//   const authToken = createJwtToken(
+//     {
+//       userId: user.id
+//     },
+//     import.meta.env.JWT_SECRET
+//   );
 
-  return { success: true, authToken };
-}
+//   return { success: true, authToken };
+// }
